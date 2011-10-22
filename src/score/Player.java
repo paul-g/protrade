99cd1279580b4cc90cc4f147d9edb101dde462cd
rb2309 <baltean.radu@gmail.com>
2011-10-22 19:28:33 +0100

    Adding Game and Player classes

---
+++ src/score/Player.java
@@ -0,0 +1,11 @@
+
+public class Player {
+	
+	private int serve_percent;
+	public boolean server;
+	public Player(int serve_percentage, boolean serving)
+	{
+		serve_percent = serve_percentage;
+		server = serving;
+	}
+}
