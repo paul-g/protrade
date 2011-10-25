015535be03b0136f6188f55ace1240e7293ecbd4
Iskander Orazbekov <io1009@imperial.ac.uk>
2011-10-25 15:43:13 +0100

    src/ui/MenuPanel.java

--- src/ui/MenuPanel.java
+++ src/ui/MenuPanel.java
@@ -13,13 +13,45 @@ public class MenuPanel {
 
 		// Main menu of the application
 		Menu menuBar = new Menu(shell, SWT.BAR);
+		
+		// File button
 		MenuItem fileMenuHeader = new MenuItem(menuBar, SWT.CASCADE);
 		fileMenuHeader.setText("&File");
 		shell.setMenuBar(menuBar);
+		
+		// File drop down menu
+		Menu fileMenu = new Menu(shell, SWT.DROP_DOWN);
+	    fileMenuHeader.setMenu(fileMenu);
 
-		// Edit menu
+	    MenuItem fileCreateItem = new MenuItem(fileMenu, SWT.PUSH);
+	    fileCreateItem.setText("New &Widget");
 
-		// About menu
+	    MenuItem fileExitItem = new MenuItem(fileMenu, SWT.PUSH);
+	    fileExitItem.setText("E&xit");
 
+		// Edit button
+		MenuItem editMenuHeader = new MenuItem(menuBar, SWT.CASCADE);
+		editMenuHeader.setText("&Edit");
+		shell.setMenuBar(menuBar);
+		
+		// Edit drop down menu
+		Menu editMenu = new Menu(shell, SWT.DROP_DOWN);
+	    editMenuHeader.setMenu(editMenu);
+
+	    MenuItem fileEditItem = new MenuItem(editMenu, SWT.PUSH);
+	    fileEditItem.setText("E&dit");
+
+		
+		// About button
+		MenuItem aboutMenuHeader = new MenuItem(menuBar, SWT.CASCADE);
+		aboutMenuHeader.setText("&About");
+		shell.setMenuBar(menuBar);
+		
+		// About drop down menu
+		Menu aboutMenu = new Menu(shell, SWT.DROP_DOWN);
+	    aboutMenuHeader.setMenu(aboutMenu);
+
+	    MenuItem aboutEditItem = new MenuItem(aboutMenu, SWT.PUSH);
+	    aboutEditItem.setText("&Creators");
 	}
 }
