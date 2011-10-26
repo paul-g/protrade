1440fa3b4e12f0bb962ffe76faeb8c1cd9fd27fd
Radu <baltean_radu@gmail.com>
2011-10-26 14:38:40 +0100

    Added OddsCalculator

---
+++ src/score/OddsCalculator.java
@@ -0,0 +1,19 @@
+
+public class OddsCalculator {
+	public int pointOdds(Player player1, Player player2)
+	{
+		return 0;
+	}
+	public int gameOdds(Game game)
+	{
+		return 0;
+	}
+	public int setOdds(Set set)
+	{
+		return 0;
+	}
+	public int matchOdds(Match match)
+	{
+		return 0;
+	}
+}
