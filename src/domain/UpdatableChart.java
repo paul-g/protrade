8b58c8942beb5eba1d536c362d59fd29eac66331
Paul Grigoras <paul.grigoras90@gmail.com>
2011-10-30 19:38:45 +0000

    A minor change to the graph.

--- src/domain/UpdatableChart.java
+++ src/domain/UpdatableChart.java
@@ -18,7 +18,9 @@ import org.swtchart.ISeries.SeriesType;
 
 public class UpdatableChart extends Chart implements UpdatableWidget {
 	private ILineSeries firstSeries;
-	private ILineSeries secondSeries;
+	//private ILineSeries secondSeries;
+	private int sampleSize = 60;
+	
 
 	public UpdatableChart(Composite parent, int style) {
 		super(parent, style);
@@ -33,13 +35,12 @@ public class UpdatableChart extends Chart implements UpdatableWidget {
 		// set colours for first series
 		Color color = new Color(Display.getDefault(), 255, 0, 0);
 		firstSeries.setLineColor(color);
-		firstSeries.setSymbolSize(4);		
+		firstSeries.setSymbolSize(4);
 		// build second series
-		secondSeries = (ILineSeries) seriesSet.createSeries(
-				SeriesType.LINE, "MA-Fast");		
+		//secondSeries = (ILineSeries) seriesSet.createSeries(SeriesType.LINE, "MA-Fast");		
 		// set series' symbol types
 		firstSeries.setSymbolType(PlotSymbolType.CROSS);
-		secondSeries.setSymbolType(PlotSymbolType.DIAMOND);		
+		//secondSeries.setSymbolType(PlotSymbolType.DIAMOND);		
 	}
 
 	/*
@@ -47,25 +48,81 @@ public class UpdatableChart extends Chart implements UpdatableWidget {
 	 */
 	public void fillData(MOddsMarketData data) {
 		// temporarily for filling charts with random data
-		Date[] xSeries = new Date[60];
-		double[] ySeries = new double[60];
-		Date[] xSeries2 = new Date[60];
-		double[] ySeries2 = new double[60];
-		Random randomGenerator = new Random();
-		Calendar calendar = Calendar.getInstance();
-		// set random values on the graphs, one every 2 seconds
-		for (int i = 0; i < 60; i++) {
-			calendar.add(Calendar.MILLISECOND, 2000); 
-			xSeries[i] = xSeries2[i] = calendar.getTime();
-			ySeries[i] = randomGenerator.nextDouble() + 1;
-			ySeries2[i] = (Math.cos(i) + 2) / 2;
-		}				
+		Date[] xSeries; // = new Date[60];
+		//double[] ySeries = new double[60];
+		//Date[] xSeries2 = new Date[60];
+		//double[] ySeries2 = new double[60];
+		
+		Date[] newXSeries;
+
+		if (firstSeries.getYSeries() != null ){
+		  Date[] prevXSeries = firstSeries.getXDateSeries();
+		  if ( prevXSeries.length < sampleSize - 1 ) {
+		    newXSeries = new Date[prevXSeries.length];
+		    int i;
+	      for (i = 0;i<prevXSeries.length;i++)
+	        newXSeries[i] = prevXSeries[i];
+	      newXSeries[i] = Calendar.getInstance().getTime();
+		  } else {
+		    newXSeries = new Date[sampleSize];
+	      int i = 1;
+	      for (;i<prevXSeries.length;i++)
+	        newXSeries[i-1] = prevXSeries[i];
+	      newXSeries[i] = Calendar.getInstance().getTime(); 
+		  }
+		} else {
+		  newXSeries = new Date[1];
+		  newXSeries[0] = Calendar.getInstance().getTime();
+		}
+		
+	/*	
+		if (firstSeries.getYSeries() != null) {
+		  xSeries = firstSeries.getXDateSeries();
+		 // xSeries2 = secondSeries.getXDateSeries();
+		  ySeries = firstSeries.getYSeries();
+		  //ySeries2 = secondSeries.getYSeries();
+		  
+		  int i;
+		  for (i = 0 ; i < 60; i++) {
+		    if (ySeries[i] < 0) break;
+		  }
+      xSeries[i] = Calendar.getInstance().getTime();
+		  // xSeries[i] = xSeries2[i] = Calendar.getInstance().getTime();
+		  if (data.getPl1Back() != null)
+		    ySeries[i] = data.getPl1Back().get(0).getI();
+		  else 
+		    ySeries[i] = 2;
+		  // testing values
+		  //ySeries2[i] = (Math.cos(i) + 2) / 2;
+		  
+		} else  {
+		  Random randomGenerator = new Random();
+	    Calendar calendar = Calendar.getInstance();   
+	    // set random values on the graphs, one every 2 seconds
+	    for (int i = 0; i < 60; i++) {
+	      if ( i < 10) {
+	        //calendar.add(Calendar.MILLISECOND, 2000); 
+          xSeries[i] = calendar.getTime();	        
+	        //xSeries[i] = xSeries2[i] = calendar.getTime();
+	        ySeries[i] = randomGenerator.nextDouble() + 1;
+	       // ySeries2[i] = (Math.cos(i) + 2) / 2;
+	      }
+	      else {
+	        xSeries[i] = calendar.getTime();
+	        //xSeries[i] = xSeries2[i] = calendar.getTime();
+          ySeries[i] = -1;
+          //ySeries2[i] = -1;
+	      }
+	    }  
+		}
+		*/
+		
 		// set first series values		
-		firstSeries.setXDateSeries(xSeries);
-		firstSeries.setYSeries(ySeries);
+		firstSeries.setXDateSeries(newXSeries);
+		//firstSeries.setYSeries(ySeries);
 		// set second series values
-		secondSeries.setXDateSeries(xSeries2);
-		secondSeries.setYSeries(ySeries2);		
+		/*secondSeries.setXDateSeries(xSeries2);
+		secondSeries.setYSeries(ySeries2);*/		
 		this.getAxisSet().adjustRange();
 	}
 
