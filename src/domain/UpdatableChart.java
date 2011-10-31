8c465637fc6454f7af3ecffbeb9d23c5bd273256
paul-g <paul.grigoras90@gmail.com>
2011-10-31 11:30:30 +0000

    Chart now displays live back value history from Betfair.

--- src/domain/UpdatableChart.java
+++ src/domain/UpdatableChart.java
@@ -17,135 +17,103 @@ import org.swtchart.ILineSeries.PlotSymbolType;
 import org.swtchart.ISeries.SeriesType;
 
 public class UpdatableChart extends Chart implements UpdatableWidget {
-	private ILineSeries firstSeries;
-	//private ILineSeries secondSeries;
-	private int sampleSize = 60;
-	
+    private ILineSeries firstSeries;
+    // private ILineSeries secondSeries;
+    private int sampleSize = 5;
 
-	public UpdatableChart(Composite parent, int style) {
-		super(parent, style);
-		setSeriesStyles();
-	}
-	
-	private void setSeriesStyles() {
-		ISeriesSet seriesSet = this.getSeriesSet();		
-		// build first series
-		firstSeries = (ILineSeries) seriesSet.createSeries(
-				SeriesType.LINE, "back odds");		
-		// set colours for first series
-		Color color = new Color(Display.getDefault(), 255, 0, 0);
-		firstSeries.setLineColor(color);
-		firstSeries.setSymbolSize(4);
-		// build second series
-		//secondSeries = (ILineSeries) seriesSet.createSeries(SeriesType.LINE, "MA-Fast");		
-		// set series' symbol types
-		firstSeries.setSymbolType(PlotSymbolType.CROSS);
-		//secondSeries.setSymbolType(PlotSymbolType.DIAMOND);		
-	}
+    public UpdatableChart(Composite parent, int style) {
+        super(parent, style);
+        setSeriesStyles();
+    }
 
-	/*
-	 * Populates the chart with the given market data
-	 */
-	public void fillData(MOddsMarketData data) {
-		// temporarily for filling charts with random data
-		Date[] xSeries; // = new Date[60];
-		//double[] ySeries = new double[60];
-		//Date[] xSeries2 = new Date[60];
-		//double[] ySeries2 = new double[60];
-		
-		Date[] newXSeries;
+    private void setSeriesStyles() {
+        ISeriesSet seriesSet = this.getSeriesSet();
+        // build first series
+        firstSeries = (ILineSeries) seriesSet.createSeries(SeriesType.LINE,
+                "back odds");
+        // set colours for first series
+        Color color = new Color(Display.getDefault(), 255, 0, 0);
+        firstSeries.setLineColor(color);
+        firstSeries.setSymbolSize(4);
+        // build second series
+        // secondSeries = (ILineSeries) seriesSet.createSeries(SeriesType.LINE,
+        // "MA-Fast");
+        // set series' symbol types
+        firstSeries.setSymbolType(PlotSymbolType.CROSS);
+        // secondSeries.setSymbolType(PlotSymbolType.DIAMOND);
+    }
 
-		if (firstSeries.getYSeries() != null ){
-		  Date[] prevXSeries = firstSeries.getXDateSeries();
-		  if ( prevXSeries.length < sampleSize - 1 ) {
-		    newXSeries = new Date[prevXSeries.length];
-		    int i;
-	      for (i = 0;i<prevXSeries.length;i++)
-	        newXSeries[i] = prevXSeries[i];
-	      newXSeries[i] = Calendar.getInstance().getTime();
-		  } else {
-		    newXSeries = new Date[sampleSize];
-	      int i = 1;
-	      for (;i<prevXSeries.length;i++)
-	        newXSeries[i-1] = prevXSeries[i];
-	      newXSeries[i] = Calendar.getInstance().getTime(); 
-		  }
-		} else {
-		  newXSeries = new Date[1];
-		  newXSeries[0] = Calendar.getInstance().getTime();
-		}
-		
-	/*	
-		if (firstSeries.getYSeries() != null) {
-		  xSeries = firstSeries.getXDateSeries();
-		 // xSeries2 = secondSeries.getXDateSeries();
-		  ySeries = firstSeries.getYSeries();
-		  //ySeries2 = secondSeries.getYSeries();
-		  
-		  int i;
-		  for (i = 0 ; i < 60; i++) {
-		    if (ySeries[i] < 0) break;
-		  }
-      xSeries[i] = Calendar.getInstance().getTime();
-		  // xSeries[i] = xSeries2[i] = Calendar.getInstance().getTime();
-		  if (data.getPl1Back() != null)
-		    ySeries[i] = data.getPl1Back().get(0).getI();
-		  else 
-		    ySeries[i] = 2;
-		  // testing values
-		  //ySeries2[i] = (Math.cos(i) + 2) / 2;
-		  
-		} else  {
-		  Random randomGenerator = new Random();
-	    Calendar calendar = Calendar.getInstance();   
-	    // set random values on the graphs, one every 2 seconds
-	    for (int i = 0; i < 60; i++) {
-	      if ( i < 10) {
-	        //calendar.add(Calendar.MILLISECOND, 2000); 
-          xSeries[i] = calendar.getTime();	        
-	        //xSeries[i] = xSeries2[i] = calendar.getTime();
-	        ySeries[i] = randomGenerator.nextDouble() + 1;
-	       // ySeries2[i] = (Math.cos(i) + 2) / 2;
-	      }
-	      else {
-	        xSeries[i] = calendar.getTime();
-	        //xSeries[i] = xSeries2[i] = calendar.getTime();
-          ySeries[i] = -1;
-          //ySeries2[i] = -1;
-	      }
-	    }  
-		}
-		*/
-		
-		// set first series values		
-		firstSeries.setXDateSeries(newXSeries);
-		//firstSeries.setYSeries(ySeries);
-		// set second series values
-		/*secondSeries.setXDateSeries(xSeries2);
-		secondSeries.setYSeries(ySeries2);*/		
-		this.getAxisSet().adjustRange();
-	}
+    /*
+     * Populates the chart with the given market data
+     */
+    public void fillData(MOddsMarketData data) {
 
-	/*
-	 * updates the chart with the new given market data
-	 */
-	public void handleUpdate(final MOddsMarketData newData) {
-		// fillData(newData);
-		final Composite comp = this.getParent();
-		final UpdatableChart chart = this;
-		if (comp != null) {
-			comp.getDisplay().asyncExec(new Runnable() {
-				@Override
-				public void run() {
-					fillData(newData);
-					if (!chart.isDisposed())
-						chart.redraw();
-					if (!comp.isDisposed())
-						comp.update();
-					//comp.getDisplay().timerExec(1000, this);
-				}
-			});
-		}
-	}
+        Date[] newXSeries;
+        double[] newYSeries;
+        
+        int i = 0;
+
+        if (firstSeries.getYSeries() != null) {
+            Date[] prevXSeries = firstSeries.getXDateSeries();
+            double[] prevYSeries = firstSeries.getYSeries();
+
+            if (prevXSeries.length < sampleSize) {
+                newXSeries = new Date[prevXSeries.length + 1];
+                newYSeries = new double[prevXSeries.length + 1];
+                for (i = 0; i < prevXSeries.length; i++){
+                    newXSeries[i] = prevXSeries[i];
+                    newYSeries[i] = prevYSeries[i];
+                }
+            } else {
+                
+                newXSeries = new Date[sampleSize];
+                newYSeries = new double[sampleSize];
+                for (i = 1; i < sampleSize; i++) {
+                    newXSeries[i - 1] = prevXSeries[i];
+                    newYSeries[i - 1] = prevYSeries[i];
+                }
+                i--;
+            }
+        } else {
+            newXSeries = new Date[1];
+            newYSeries = new double[1];
+        }
+
+        newXSeries[i] = Calendar.getInstance().getTime();
+        if ( data.getPl1Back() != null)
+            newYSeries[i] = data.getPl1Back().get(0).getI();
+        else
+            if ( i > 0 )
+                newYSeries[i] = newYSeries[i-1];
+            else
+                newYSeries[i] = 0;
+
+        // set first series values
+        firstSeries.setXDateSeries(newXSeries);
+        firstSeries.setYSeries(newYSeries);
+        this.getAxisSet().adjustRange();
+    }
+
+    /*
+     * updates the chart with the new given market data
+     */
+    public void handleUpdate(final MOddsMarketData newData) {
+        // fillData(newData);
+        final Composite comp = this.getParent();
+        final UpdatableChart chart = this;
+        if (comp != null) {
+            comp.getDisplay().asyncExec(new Runnable() {
+                @Override
+                public void run() {
+                    fillData(newData);
+                    if (!chart.isDisposed())
+                        chart.redraw();
+                    if (!comp.isDisposed())
+                        comp.update();
+                    // comp.getDisplay().timerExec(1000, this);
+                }
+            });
+        }
+    }
 
 }
