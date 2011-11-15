540de1b889f7e65fc992c30989b6235069d5ee80
paul-g <paul.grigoras90@gmail.com>
2011-11-15 15:52:54 +0000

    Added SetScore class and some functionality to the random data generator.

--- src/org/ic/tennistrader/domain/match/Score.java
+++ src/org/ic/tennistrader/domain/match/Score.java
@@ -1,5 +1,24 @@
 package org.ic.tennistrader.domain.match;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Score {
-//    List<Pair> scores = 
+    private int playerOnePoints;
+    private int playerTwoPoints;
+    
+    private List<SetScore> scores = new ArrayList<SetScore>();
+    
+    public Score(){
+    }
+    
+    public void addPlayerOnePoint(){
+        
+    }
+    
+    public void addPlayerTwoPoint(){
+        
+    }
+    
+    
 }
