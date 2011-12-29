4dec26d5dd7020d7760f618c7b20ee8b8d45fc59
Iskander Orazbekov <io1009@imperial.ac.uk>
2011-12-30 00:48:52 +0400

    Minor changes

--- src/org/ic/tennistrader/ui/LoginShell.java
+++ src/org/ic/tennistrader/ui/LoginShell.java
@@ -251,7 +251,7 @@ public class LoginShell {
 			@Override
 			public void run() {
 				try {
-					Thread.sleep(1500);
+					Thread.sleep(2000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
