5f25e79f2960ea2fc7a2f4b6ce4af84e5651c2d9
Iskander Orazbekov <io1009@imperial.ac.uk>
2011-12-30 19:01:19 +0400

    Restyled the profile page a bit, still needs more refurbishment

--- src/org/ic/tennistrader/ui/ProfileWindow.java
+++ src/org/ic/tennistrader/ui/ProfileWindow.java
@@ -1,7 +1,8 @@
 package org.ic.tennistrader.ui;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
@@ -13,6 +14,7 @@ public class ProfileWindow {
 	private ProfileData profileData;
 	private Shell shell;
 	private Display display;
+	private GridData gd;
 	
 	public static void main (String[] args) {
 		Display d = new Display();
@@ -22,46 +24,74 @@ public class ProfileWindow {
 	
 	/** Constructor */
 	public ProfileWindow(Display display, ProfileData profdata) {
+		
+		/* Initialisation */
 		this.display = display;
 		shell = new Shell(display,SWT.SHELL_TRIM);
 		profileData = profdata;
-		shell.setLayout(new FillLayout());
+		
+		/* Grid Layout of the page */
+		GridLayout gl = new GridLayout(2,true);
+		gd = new GridData(SWT.CENTER,SWT.CENTER,true,true);
+		gd.minimumWidth = 150;
+		
+		/* Shell Layout */
+		shell.setLayout(gl);
 		shell.setText("Profile");
-		shell.setSize(200,200);
+		shell.setSize(400,300);
 		shell.setLocation(1100,80);
-		Label profData = new Label(shell,SWT.BORDER);
-		profData.setText("Hello!");
-		textProfile();
+		
+		/* Labels and Shell opening */
+		fillProfile(gd);
 		shell.open();
 	}
 	
 	/** Text representation of the profile */
-	private String textProfile() {
-		String res ="";
+	private void fillProfile(GridData gd) {
+		AccountFunds af = null;
 		try {
-			AccountFunds af = profileData.getUkAccountFunds();
-			res =
-				"Username : " + Main.username +
-				"\nBetfair points : " + af.getBetfairPoints() +
-				"\nCurrent balance : " + af.getBalance() +
-				"\nAvailable balance : " + af.getAvailable() +
-				"\nCredit limit : " + af.getCreditLimit() +
-				"\nExposure : " + af.getExposure() +
-				"\nExposure limit : " + af.getExposureLimit();
+			af = profileData.getUkAccountFunds();
 		} catch (Exception e) {
 			e.getMessage();
 		}
-		return res;
+		addLabel("USERNAME",Main.username);
+		addLabel("BETFAIR POINTS",af.getBetfairPoints()+"");
+		addLabel("CURRENT BALANCE",af.getBalance()+"");
+		addLabel("AVAILABLE BALANCE",af.getAvailable()+"");
+		addLabel("CREDIT LIMIT",af.getCreditLimit()+"");
+		addLabel("EXPOSURE",af.getExposure()+"");
+		addLabel("EXPOSURE LIMIT",af.getExposureLimit()+"");
+//		addLabel("USERNAME","");
+//		addLabel("BETFAIR POINTS","");
+//		addLabel("CURRENT BALANCE","");
+//		addLabel("AVAILABLE BALANCE","");
+//		addLabel("CREDIT LIMIT","");
+//		addLabel("EXPOSURE","");
+//		addLabel("EXPOSURE LIMIT","");
 	}
 	
+	/** Method for label addition */
+	private void addLabel(String key,String value) {
+		Label nkey = new Label(shell, SWT.BORDER);
+		nkey.setText(key);
+		nkey.setLayoutData(gd);
+		Label nvalue = new Label(shell, SWT.BORDER);
+		nvalue.setText(value);
+		nvalue.setLayoutData(gd);
+		nvalue.setAlignment(SWT.CENTER);
+	}
+	
+	/** External dispose check */
 	public boolean isDisposed() {
 		return shell.isDisposed();
 	}
 	
+	/** External method for forcing */
 	public void forceActive() {
 		shell.forceActive();
 	}
 	
+	/** Method for keeping the shell */
 	public void stay() {
 		while(!shell.isDisposed()) {
 			if (!display.readAndDispatch() ) display.sleep();
